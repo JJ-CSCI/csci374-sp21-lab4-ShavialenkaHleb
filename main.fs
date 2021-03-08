22a893dcfeb79ae664b8c09f787cb0d95c4ce8bf
ShavialenkaHleb <boss530@yandex.ru>
2021-03-08 23:09:16 +0000

    d

--- main.fs
+++ main.fs
@@ -2,20 +2,32 @@ module Assignment
 
 // Problem 1
 let rec prod (lst:int list) =
-    // write your solution here
-    0
+match lst with
+| [] -> 1
+| head :: tail -> head * product tail
+
 
 // Problem 2
 let rec map f (lst:int list) =
-    // write your solution here
-    []
+   match f with
+      [] -> []
+    | head::tail -> (f head)::(map f tail)
 
 // Problem 3
 let rec odd (lst:int list) =
-    // write your solution here
-    lst
+match lst with
+| [] -> []
+| head :: tail ->
+if head % 2 = 0 then
+head :: odd tail
+
 
 // Problem 4
 let rec filter f lst =
-    // write your solution here
-    lst
+match lst with
+| [] -> []
+| head :: tail ->
+if f head then
+head :: filter f tail
+else
+filter f tail
